356a1ac041a21482c2f99477eff7d423940aa532
Shuhey-Y <95632335+Shuhey-Y@users.noreply.github.com>
2022-03-29 02:15:34 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,97 @@
+$(function () {
+  $('.hamburger').click(function () {
+    $(this).toggleClass('active');
+
+    if ($(this).hasClass('active')) {
+      $('.header_list').addClass('active');
+    } else {
+      $('.header_list').removeClass('active');
+    }
+  });
+});
+
+
+
+  let swipeOption = {
+    loop: true,
+  effect: 'fade',
+  autoplay: {
+    delay: 4000,
+  disableOnInteraction: false,
+    },
+  speed: 2000,
+  pagination: {
+    el: '.swiper-pagination',
+  clickable: true,
+    }
+  }
+  new Swiper('.swiper-container-4', swipeOption);
+
+
+//lightbox オプションの設定※https://lokeshdhakar.com/projects/lightbox2/#options参照
+
+lightbox.option({
+  'wrapAround': true,//グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
+  'albumLabel': ' %1 / total %2 '//合計枚数中現在何枚目かというキャプションの見せ方を変更できる
+})
+
+//ふわっと見せるためのJS。3-5-3 ページが読み込まれたらすぐに動かしたい&画面をスクロールをしたら動かしたい場合内のソースコード使用
+
+function fadeAnime() {
+  // flipLeft
+  $('.gallery li').each(function () {
+    var elemPos = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('flipLeft');
+    } else {
+      $(this).removeClass('flipLeft');
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまでページが読み込まれたらすぐに動かしたい場合の記述
+
+
+function GethashID(hashIDName) {
+  if (hashIDName) {
+    //タブ設定
+    $('.tab li').find('a').each(function () { //タブ内のaタグ全てを取得
+      var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得
+      if (idName == hashIDName) { //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+        var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+        $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+        $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+        //表示させるエリア設定
+        $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+        $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加
+      }
+    });
+  }
+}
+
+
+
+
+
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  speed: 500,//スライドのスピード。初期値は300。
+  slidesToShow: 3,//スライドを画面に3枚見せる
+  slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  centerMode: true,//要素を中央ぞろえにする
+  variableWidth: true,//幅の違う画像の高さを揃えて表示
+  dots: true,//下部ドットナビゲーションの表示
+});
